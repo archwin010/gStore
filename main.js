d1f44eb94d42c659281d06fde6fbe0f91aaff518
Godwin <archibong_godwin@yahoo.com>
2023-10-19 22:40:12 +0100

    first of the day

--- main.js
+++ main.js
@@ -11,8 +11,31 @@ function copyMenu() {
     navPlace.innerHTML = mainNav.innerHTML;
 
     // copy .header-top .wrapper to .thetop-nav
-    var topNav = document.querySelector('.header-top');
+    var topNav = document.querySelector('.header-top .wrapper');
     var topPlace = document.querySelector('.off-canvas .thetop-nav')
     topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector('.trigger'),
+    closeButton = document.querySelector('.t-close'),
+    addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function() {
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click', function() {
+    addclass.classList.remove('showmenu')
+})
+
+
+// show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
