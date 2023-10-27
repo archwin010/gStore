50dec5dc8d4e79845b8b62b5b61770a30d201383
Godwin <archibong_godwin@yahoo.com>
2023-10-27 16:34:25 +0100

    more functionalities and interactivity to be added

--- main.js
+++ main.js
@@ -60,3 +60,11 @@ searchButton.addEventListener('click', function() {
 tClose.addEventListener('click', function() {
     showClass.classList.remove('showsearch')
 })
+
+
+// show dpt menu
+const dptBUtton = document.querySelector('.dpt-cat .dpt-trigger'),
+    dptClass = document.querySelector('.site');
+dptBUtton.addEventListener('click', function() {
+    dptClass.classList.toggle('showdpt')
+})
