ff39005692cc76500d3463967162f2bf37d1b6ab
Godwin <archibong_godwin@yahoo.com>
2023-11-06 13:14:56 +0100

    added styling and interactivity to the women section on the category page

--- main.js
+++ main.js
@@ -104,3 +104,24 @@ for (let x = 0; x < stocks.length; x++) {
 
     stocks[x].querySelector('.available').style.width = percent + '%';
 }
+
+// for filter on the category page
+const FtoShow = '.filter';
+const Fpopup = document.querySelector(FtoShow);
+const Ftrigger = document.querySelector('.filter-trigger');
+
+Ftrigger.addEventListener('click', () => {
+    setTimeout(() => {
+        if(!Fpopup.classList.contains('show')) {
+            Fpopup.classList.add('show')
+        }
+    }, 250 )
+})
+
+// auto close by click outside .filter
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(FtoShow);
+    if(!isClosest && Fpopup.classList.contains('show')) {
+        Fpopup.classList.remove('show')
+    }
+})
